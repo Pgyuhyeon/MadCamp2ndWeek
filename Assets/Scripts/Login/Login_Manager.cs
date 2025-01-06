f351d6fcdc8309544a94e95a404354bd0e91309f
Pgyuhyeon <robin960760@gmail.com>
2025-01-06 15:39:42 +0900

    merge2

---
+++ Assets/Scripts/Login/Login_Manager.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.Collections;
+using UnityEngine.Networking;
+using Unity.VisualScripting;
+
+public class Login_Manager : MonoBehaviour
+{
+    public TMP_InputField Username_input;
+    public TMP_InputField Password_input;
+    public Button App_Login_Button;
+    public Button App_Register_Button;
+    public GameObject LoginPanel;
+    public GameObject Player;
+    public GameObject RegisterPanel;
+    private string Login_server_url = "http://43.202.48.66:3000/login";
+
+    void Awake()
+    {
+        PlayerPrefs.SetInt("IsLoggedIn", 0); // 로그인 상태 초기화
+        PlayerPrefs.Save();
+    }
+    void Start()
+    {
+        if(PlayerPrefs.GetInt("IsLoggedIn", 0)==1)
+        {
+            LoginPanel.SetActive(false);
+            Player.SetActive(true);
+        }
+        else
+        {
+            App_Login_Button.onClick.AddListener(App_Login_button_click);
+            Player.SetActive(false);
+        }
+    }
+
+    public void clear_input()
+    {
+        Username_input.text="";
+        Password_input.text="";
+    }
+
+    public void App_Login_button_click()
+    {
+        string username = Username_input.text;
+        string password = Password_input.text;
+        Debug.Log("I clicked Login Button");
+        StartCoroutine(Send_Login_Request(username, password));
+    }
+
+    private IEnumerator Send_Login_Request(string username, string password)
+    {
+        string Login_json_data = $"{{\"username\":\"{username}\",\"password\":\"{password}\"}}";
+        UnityWebRequest request = new UnityWebRequest(Login_server_url, "POST");
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(Login_json_data);
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+        // 응답 처리
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            if (request.responseCode == 200)
+            {
+                Debug.Log("Login Successful");
+                PlayerPrefs.SetInt("IsLoggedIn", 1);
+                PlayerPrefs.Save();
+                LoginPanel.SetActive(false);
+                Player.SetActive(true);
+            }
+            else if (request.responseCode == 401)
+            {
+                Debug.Log("Invalid username or password.");
+            }
+        }
+        else
+        {
+            Debug.Log("Error: " + request.error);
+        }
+    }
+    public void App_Register_button_click()
+    {
+        Debug.Log("I clicked Register Button");
+        clear_input();
+        LoginPanel.SetActive(false);
+        RegisterPanel.SetActive(true);
+    }
+}
